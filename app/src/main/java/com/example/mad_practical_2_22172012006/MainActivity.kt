c3932735f00f0d728f76f3e6d101749e078f44e2
student <student@gnu.ac.in>
2023-08-02 08:36:50 +0530

    2-3-23

--- app/src/main/java/com/example/mad_practical_2_22172012006/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_2_22172012006/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +18,40 @@ class MainActivity : AppCompatActivity() {
     {
         Log.i(TAG, message)
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        val obj:ConstraintLayout? = findViewById(R.id.mainconstraint)
+        if(obj!==null)
+        {
+            Snackbar.make(obj,message,Snackbar.LENGTH_SHORT).show()
+        }
     }
 
     override fun onStart() {
         super.onStart()
         showMessage("onStart method called")
-        onRe
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showMessage("onResume method called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        showMessage("onPause method called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        showMessage("onStop method called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        showMessage("onDestroy method called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        showMessage("onRestart method called")
     }
 }
